2c0be04cbec16a0fcccf5679d5861c69afc83d84
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-02 21:29:04 -0300

    Definido layout básico, contador

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            counter: 0
+        };
+
+        this.addCounter = this.addCounter.bind(this);
+    }
+
+    addCounter() {
+        var count = this.state.counter;
+        count++;
+        this.setState({counter:count});
+    }
+
     render() {
         return (
-            <p>Hola, mundo!</p>
+            <div>
+                <button className="btn btn-primary" onClick={this.addCounter}>
+                    <i className="fas fa-plus fa-fw"></i> Add To Counter
+                </button>
+                <hr/>
+                <p>Counter: {this.state.counter}</p>
+            </div>
         );
     }
 }
